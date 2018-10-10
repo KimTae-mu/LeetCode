7d37a26029cb21a266cf12443206bbc36b52f2d1
穆国超 <muguochao@qijiayoudao.com>
2018-10-10 19:30:34 +0800

    LeetCode

---
+++ src/main/java/com/alva/LeetCode/LongestSubString.java
@@ -0,0 +1,48 @@
+package com.alva.LeetCode;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <一句话描述>,
+ * <详细介绍>,
+ *
+ * @author 穆国超
+ * @since 设计wiki | 需求wiki
+ */
+public class LongestSubString {
+
+/*    private int lengthOfLongestSubstring(String s) {
+        String news = "";
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (news.contains(String.valueOf(c))) {
+                news += String.valueOf(c);
+                i++;
+            }
+        }
+    }*/
+
+    private int lengthOfLongestSubstringWithMap(String s) {
+        if (s.length() <= 1) return s.length();
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - j + 1);
+        }
+        return max;
+    }
+
+
+    @Test
+    public void testLengthOfString() {
+        String s = "abcabcd";
+        System.out.println(lengthOfLongestSubstringWithMap(s));
+    }
+}
