1b3aa2aeb2c56ccc9249fcd881b5de4fa5efb79a
穆国超 <muguochao@qijiayoudao.com>
2018-10-20 20:34:04 +0800

    LeetCode

---
+++ src/main/java/com/alva/LeetCode/ZigZagConversion.java
@@ -0,0 +1,39 @@
+package com.alva.LeetCode;
+
+/**
+ * <一句话描述>,
+ * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+ * <p>
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * And then read line by line: "PAHNAPLSIIGYIR"
+ * <p>
+ * Write the code that will take a string and make this conversion given a number of rows:
+ * <p>
+ * string convert(string s, int numRows);
+ * <详细介绍>,
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 3
+ * Output: "PAHNAPLSIIGYIR"
+ * Example 2:
+ * <p>
+ * Input: s = "PAYPALISHIRING", numRows = 4
+ * Output: "PINALSIGYAHRPI"
+ * Explanation:
+ * <p>
+ * P     I    N
+ * A   L S  I G
+ * Y A   H R
+ * P     I
+ *
+ * @author 穆国超
+ * @since 设计wiki | 需求wiki
+ */
+public class ZigZagConversion {
+    public String convert(String s, int numRows) {
+
+    }
+}
