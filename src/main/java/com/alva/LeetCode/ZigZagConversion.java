129b653e56a1e580bb98f480cd215158ff079c55
穆国超 <muguochao@qijiayoudao.com>
2018-10-23 10:33:26 +0800

    LeetCode

--- src/main/java/com/alva/LeetCode/ZigZagConversion.java
+++ src/main/java/com/alva/LeetCode/ZigZagConversion.java
@@ -1,5 +1,7 @@
 package com.alva.LeetCode;
 
+import org.junit.Test;
+
 /**
  * <一句话描述>,
  * The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
@@ -34,6 +36,38 @@ package com.alva.LeetCode;
  */
 public class ZigZagConversion {
     public String convert(String s, int numRows) {
+        if (s == null || numRows <= 1) {
+            return s;
+        }
+        char[] chars = s.toCharArray();
+        int len = chars.length;
 
+        StringBuffer[] sb = new StringBuffer[numRows];
+        for (int i = 0; i < sb.length; i++) {
+            sb[i] = new StringBuffer();
+        }
+
+        int i = 0;
+        while (i < len) {
+            for (int idx = 0; idx < numRows && i < len; idx++) {
+                sb[idx].append(chars[i++]);
+            }
+            for (int idx = numRows - 2; idx >= 1 && i < len; idx--) {
+                sb[idx].append(chars[i++]);
+            }
+        }
+        for (int idx = 1; idx < sb.length; idx++) {
+            sb[0].append(sb[idx]);
+        }
+        return sb[0].toString();
+    }
+
+    /**
+     * 对题目理解有误,题目仅要求生成一行,对题目的理解造成了误解,复杂化
+     */
+    @Test
+    public void test(){
+        String s = "PAYPALISHIRING";
+        System.out.println(convert(s,4));
     }
 }
