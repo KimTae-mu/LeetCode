03b21f8091934a82f36ca6d3c30d433e71f9ee7d
穆国超 <muguochao@qijiayoudao.com>
2018-10-25 19:01:23 +0800

    Leetcode

---
+++ src/main/java/com/alva/LeetCode/RegularExpressionMatching.java
@@ -0,0 +1,58 @@
+package com.alva.LeetCode;
+
+/**
+ * <一句话描述>,
+ * <p>
+ * Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+ * <p>
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * The matching should cover the entire input string (not partial).
+ * <p>
+ * Note:
+ * <p>
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters like . or *.
+ * <p>
+ * <详细介绍>,
+ * <p>
+ * Example 1:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * Example 2:
+ * <p>
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+ * Example 3:
+ * <p>
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * Example 4:
+ * <p>
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+ * Example 5:
+ * <p>
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ *
+ * @author 穆国超
+ * @since 设计wiki | 需求wiki
+ */
+public class RegularExpressionMatching {
+}
