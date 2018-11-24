7f58b8f43a79aea01c25b30dab3d64d335bb44cd
KimTae-mu <1160846772@qq.com>
2018-11-24 22:11:48 +0800

    integer to Roman

---
+++ src/main/java/com/alva/LeetCode/IntegertoRoman.java
@@ -0,0 +1,84 @@
+package com.alva.LeetCode;
+
+import org.junit.Test;
+
+/**
+ * <一句话描述>,
+ * <p>
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ * For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+ * <p>
+ * <详细介绍>,
+ * For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+ * <p>
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ * <p>
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+ *
+ * @author 穆国超
+ * @since 设计wiki | 需求wiki
+ */
+public class IntegertoRoman {
+
+    public String intToRoman(int num) {
+        StringBuilder result = new StringBuilder();
+        while (num > 0) {
+            for (Roman temp : Roman.values()) {
+                if (num >= temp.getKey()) {
+                    num -= temp.getKey();
+                    result.append(temp.getValue());
+                    break;
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    enum Roman {
+        M(1000, "M"),
+        CM(900, "CM"),
+        D(500, "D"),
+        CD(400, "CD"),
+        C(100, "C"),
+        XC(90, "XC"),
+        L(50, "L"),
+        XL(40, "XL"),
+        X(10, "X"),
+        IX(9, "IX"),
+        V(5, "V"),
+        IV(4, "IV"),
+        I(1, "I");
+
+        private final Integer key;
+
+        private final String value;
+
+        Roman(Integer key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public Integer getKey() {
+            return key;
+        }
+    }
+
+    @Test
+    public void test() {
+        System.out.println(intToRoman(20));
+    }
+}
