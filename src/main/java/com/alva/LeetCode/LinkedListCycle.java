f0038d63b7da67e4688ce6966eb39307c638d660
穆国超 <muguochao@qijiayoudao.com>
2018-11-29 22:02:39 +0800

    LinkedListCycle

--- src/main/java/com/alva/LeetCode/LinkedListCycle.java
+++ src/main/java/com/alva/LeetCode/LinkedListCycle.java
@@ -18,6 +18,12 @@ import java.util.Set;
  */
 public class LinkedListCycle {
 
+    /**
+     * 使用一个set集合来判断是否有重复元素从而判断是否有环
+     *
+     * @param head
+     * @return
+     */
     public boolean hasCycle(ListNode head) {
         Set<ListNode> set = new HashSet<>();
 
@@ -28,6 +34,46 @@ public class LinkedListCycle {
             set.add(head);
             head = head.next;
         }
+        return false;
+    }
+
+    /**
+     * 分别一个快指针和慢指针,快指针走的路程是慢指针的两倍,若两指针相遇则代表存在环,
+     * 若不相遇则不存在环;
+     * 存在环时,快慢指针相遇的位置与链表头结点分别相距环起始点的位置距离相同,
+     * 则一次遍历头结点与相遇结点,两者相遇点即为环开始点.
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycleII(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode meet = null;
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+            if (fast == slow) {
+                meet = fast;
+                break;
+            }
+        }
+
+        if (meet == null) {
+            return false;
+        }
+        while (head != null && meet != null) {
+            if (head == meet) {
+                return true;
+            }
+            head = head.next;
+            meet = meet.next;
+        }
+
         return false;
     }
 }
